d4004526fa4936d7eacc98e015a5851e3808aa80
Timotee BABOULIN <t.baboulin@ensicar.lan>
2023-05-11 14:32:51 +0200

    Sauvegarde du son

--- Assets/02 - Timotee/00 - Prototype/01 - Scripts/Menus/MainMenu.cs
+++ Assets/02 - Timotee/00 - Prototype/01 - Scripts/Menus/MainMenu.cs
@@ -1,8 +1,10 @@
+using System;
 using Managers;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.Playables;
+using UnityEngine.UI;
 
 namespace Menus{
 	public class MainMenu : MonoBehaviour{
@@ -13,16 +15,55 @@ namespace Menus{
 		public PlayableDirector TimelineDirector;
 		public PlayableAsset StartGameTimeline;
 
+		public string MasterVolumePlayerPrefName;
+		public string BGMVolumePlayerPrefName;
+		public string SFXVolumePlayerPrefName;
+
+		public Slider MasterVolumeSlider;
+		public Slider BGMVolumeSlider;
+		public Slider SFXVolumeSlider;
+		
 		public GameObject GameOverMenu;
+
+		private void Start(){
+			if (PlayerPrefs.HasKey(MasterVolumePlayerPrefName))
+				LoadVolumes();
+			else
+				InitializeVolumes();
+		}
 
 		private void OnEnable(){
 			GameManager.OnGameLost += GameOver;
 			GameManager.OnRestart += Restart;
 		}
 
+		private void InitializeVolumes(){
+			PlayerPrefs.SetFloat(MasterVolumePlayerPrefName, 1);
+			PlayerPrefs.SetFloat(BGMVolumePlayerPrefName, 1);
+			PlayerPrefs.SetFloat(SFXVolumePlayerPrefName, 1);
+		}
+		
+		private void LoadVolumes(){
+			float master, sfx, bgm;
+			
+			master = PlayerPrefs.GetFloat(MasterVolumePlayerPrefName);
+			bgm = PlayerPrefs.GetFloat(BGMVolumePlayerPrefName);
+			sfx = PlayerPrefs.GetFloat(SFXVolumePlayerPrefName);
+
+			MasterVolumeSlider.value = master;
+			BGMVolumeSlider.value = bgm;
+			SFXVolumeSlider.value = sfx;
+			
+			AudioMixer.SetFloat("MasterVolume", Mathf.Log10(master) * 20);
+			AudioMixer.SetFloat("BGMVolume", Mathf.Log10(bgm) * 20);
+			AudioMixer.SetFloat("SFXVolume", Mathf.Log10(sfx) * 20);
+		}
+
 		private void OnDisable(){
 			GameManager.OnGameLost -= GameOver;
 			GameManager.OnRestart -= Restart;
+			
+			PlayerPrefs.Save();
 		}
 
 		public void StartGame(){
@@ -32,16 +73,19 @@ namespace Menus{
 			TimelineDirector.Play();
 		}
 
+		public void ChangeVolumeMaster(float value){
+			AudioMixer.SetFloat("MasterVolume", Mathf.Log10(value) * 20);
+			PlayerPrefs.SetFloat(MasterVolumePlayerPrefName, value);
+		}
+		
 		public void ChangeVolumeBGM(float value){
 			AudioMixer.SetFloat("BGMVolume", Mathf.Log10(value) * 20);
+			PlayerPrefs.SetFloat(BGMVolumePlayerPrefName, value);
 		}
 
 		public void ChangeVolumeSFX(float value){
 			AudioMixer.SetFloat("SFXVolume", Mathf.Log10(value) * 20);
-		}
-
-		public void ChangeVolumeMaster(float value){
-			AudioMixer.SetFloat("MasterVolume", Mathf.Log10(value) * 20);
+			PlayerPrefs.SetFloat(SFXVolumePlayerPrefName, value);
 		}
 
 		public void ExitGame(){
