7431b5434dbe17f243ef94b4c218244e3aea262b
Timotee BABOULIN <t.baboulin@ensicar.lan>
2023-05-17 17:01:36 +0200

    Trees be rotating

--- Assets/02 - Timotee/00 - Prototype/01 - Scripts/Map/Tree.cs
+++ Assets/02 - Timotee/00 - Prototype/01 - Scripts/Map/Tree.cs
@@ -10,11 +10,13 @@ namespace Obstacles{
 				_tree.transform.SetParent(StaticTreeArray.Transform);
 			}
 
-			
 			_tree = StaticTreeArray.GenerateTree();
 			_tree.SetActive(true);
 			_tree.transform.SetParent(transform);
 			_tree.transform.position = transform.position;
+
+			var random = Random.Range(0f, 180f);
+			_tree.transform.rotation = Quaternion.Euler(0, random, 0);
 		}
 	}
 }
